c3439a114490d58b9104b1169652f124bc373da9
salaheddineElbaghdadi <s.elbaghdadi@outlook.com>
2019-07-19 11:45:51 +0100

    Added Loans view

---
+++ LibraryManagementSystem/Controllers/LoansController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagementSystem.Models.Library;
+using LibraryManagementSystem.ViewModels.Loans;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class LoansController : Controller
+    {
+        // GET: Loans
+        public ActionResult Index()
+        {
+            LibraryDal dal = new LibraryDal();
+            LoansViewModel model = new LoansViewModel();
+
+            model.Loans = dal.LoansList();
+            model.BookTitles = new List<string>();
+            model.ClientFirstNames = new List<string>();
+            model.ClientLastNames = new List<string>(); 
+
+            foreach(ClientBook loan in model.Loans)
+            {
+                Book book = dal.GetBookById(loan.BookId);
+                Client client = dal.GetClientById(loan.ClientId);
+                model.BookTitles.Add(book.Title);
+                model.ClientFirstNames.Add(client.FirstName);
+                model.ClientLastNames.Add(client.LastName);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ LibraryManagementSystem/ViewModels/Loans/LoansViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibraryManagementSystem.Models.Library;
+
+namespace LibraryManagementSystem.ViewModels.Loans
+{
+    public class LoansViewModel
+    {
+        public List<ClientBook> Loans { get; set; }
+        public List<string> BookTitles { get; set; }
+        public List<string> ClientFirstNames { get; set; }
+        public List<string> ClientLastNames { get; set; }
+    }
+}
