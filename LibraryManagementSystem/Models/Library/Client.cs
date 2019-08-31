928f111b3413a13b950f4056bb7b967cdced5a42
salaheddineElbaghdadi <s.elbaghdadi@outlook.com>
2019-08-31 01:08:48 +0100

    Working on clients validation

--- LibraryManagementSystem/Models/Library/Client.cs
+++ LibraryManagementSystem/Models/Library/Client.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace LibraryManagementSystem.Models.Library
 {
@@ -10,9 +10,13 @@ namespace LibraryManagementSystem.Models.Library
     public class Client
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string CIN { get; set; }
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")]
         public string Email { get; set; }
 
         /// <summary>
@@ -22,6 +26,7 @@ namespace LibraryManagementSystem.Models.Library
         /// <summary>
         /// The Category of the client
         /// </summary>
+        [Required]
         public virtual ClientCategory Category { get; set; }
 
         public Client()
